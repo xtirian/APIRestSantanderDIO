21768a219d194078d7f679f1e7a3d626192ff086
xTirian <tirianx@yahoo.com>
2023-10-22 16:19:57 -0300

    add origin

---
+++ src/test/java/dio/santander/APIRest/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.santander.APIRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
